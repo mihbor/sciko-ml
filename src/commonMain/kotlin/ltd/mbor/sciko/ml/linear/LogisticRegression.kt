f086bdca69ef34f2239b9f098e6120ae42e2159c
Michal Borowiecki <mihbor@users.noreply.github.com>
2025-01-04 15:40:05 +0000

    LogisticRegression tweaks

--- src/commonMain/kotlin/ltd/mbor/sciko/ml/linear/LogisticRegression.kt
+++ src/commonMain/kotlin/ltd/mbor/sciko/ml/linear/LogisticRegression.kt
@@ -1,69 +1,69 @@
 package ltd.mbor.sciko.ml.linear
 
 import ltd.mbor.sciko.ml.dataframe.DataFrame
-import org.jetbrains.kotlinx.multik.api.Multik
+import org.jetbrains.kotlinx.multik.api.KEEngineType
 import org.jetbrains.kotlinx.multik.api.linalg.dot
 import org.jetbrains.kotlinx.multik.api.mk
 import org.jetbrains.kotlinx.multik.api.ndarray
-import org.jetbrains.kotlinx.multik.api.ones
+import org.jetbrains.kotlinx.multik.api.zeros
+import org.jetbrains.kotlinx.multik.default.math.DefaultMathEx.exp
 import org.jetbrains.kotlinx.multik.ndarray.data.D1
 import org.jetbrains.kotlinx.multik.ndarray.data.D2
 import org.jetbrains.kotlinx.multik.ndarray.data.MultiArray
-import org.jetbrains.kotlinx.multik.ndarray.data.get
-import org.jetbrains.kotlinx.multik.ndarray.operations.exp
-import org.jetbrains.kotlinx.multik.ndarray.operations.minus
-import org.jetbrains.kotlinx.multik.ndarray.operations.plus
-import org.jetbrains.kotlinx.multik.ndarray.operations.times
+import org.jetbrains.kotlinx.multik.ndarray.operations.*
 
 class LogisticRegression {
-    var weights: MultiArray<Double, D1>? = null
-    var bias: Double = 0.0
-    private val learningRate = 0.01
-    private val numIterations = 1000
+  var weights: MultiArray<Double, D1>? = null
+  var bias: Double = 0.0
+  private val learningRate = 0.01
+  private val numIterations = 1000
+  init {
+    mk.setEngine(KEEngineType)
+  }
 
-    fun fit(X: MultiArray<Double, D2>, y: MultiArray<Double, D1>): LogisticRegression {
-        val nSamples = X.shape[0]
-        val nFeatures = X.shape[1]
-        weights = mk.zeros(nFeatures)
-        bias = 0.0
+  fun fit(X: MultiArray<Double, D2>, y: MultiArray<Double, D1>): LogisticRegression {
+    val nSamples = X.shape[0]
+    val nFeatures = X.shape[1]
+    weights = mk.zeros<Double>(nFeatures)
+    bias = 0.0
 
-        for (i in 0 until numIterations) {
-            val linearModel = X.dot(weights!!) + bias
-            val yPredicted = sigmoid(linearModel)
+    for (i in 0 until numIterations) {
+      val linearModel = X.dot(weights!!) + bias
+      val yPredicted = sigmoid(linearModel)
 
-            val dw = (1.0 / nSamples) * (X.transpose().dot(yPredicted - y))
-            val db = (1.0 / nSamples) * (yPredicted - y).sum()
+      val dw = (1.0 / nSamples) * (X.transpose().dot(yPredicted - y))
+      val db = (1.0 / nSamples) * (yPredicted - y).sum()
 
-            weights = weights!! - learningRate * dw
-            bias -= learningRate * db
-        }
-
-        return this
+      weights = weights!! - learningRate * dw
+      bias -= learningRate * db
     }
 
-    fun fit(X: DataFrame, y: DataFrame.Column<Double>): LogisticRegression {
-        return fit(mk.ndarray(X.columns.map { (it as DataFrame.Column<Double>).values }).transpose(), mk.ndarray(y.values))
-    }
+    return this
+  }
 
-    fun predict(X: MultiArray<Double, D2>): MultiArray<Double, D1> {
-        val linearModel = X.dot(weights!!) + bias
-        return sigmoid(linearModel).map { if (it >= 0.5) 1.0 else 0.0 }
-    }
+  fun fit(X: DataFrame, y: DataFrame.Column<Double>): LogisticRegression {
+    return fit(mk.ndarray(X.columns.map { (it as DataFrame.Column<Double>).values }).transpose(), mk.ndarray(y.values))
+  }
 
-    fun predict(X: DataFrame): MultiArray<Double, D1> {
-        return predict(mk.ndarray(X.columns.map { (it as DataFrame.Column<Double>).values }).transpose())
-    }
+  fun predict(X: MultiArray<Double, D2>): MultiArray<Double, D1> {
+    val linearModel = X.dot(weights!!) + bias
+    return sigmoid(linearModel).map { if (it >= 0.5) 1.0 else 0.0 }
+  }
 
-    fun predictProbability(X: MultiArray<Double, D2>): MultiArray<Double, D1> {
-        val linearModel = X.dot(weights!!) + bias
-        return sigmoid(linearModel)
-    }
+  fun predict(X: DataFrame): MultiArray<Double, D1> {
+    return predict(mk.ndarray(X.columns.map { (it as DataFrame.Column<Double>).values }).transpose())
+  }
 
-    fun predictProbability(X: DataFrame): MultiArray<Double, D1> {
-        return predictProbability(mk.ndarray(X.columns.map { (it as DataFrame.Column<Double>).values }).transpose())
-    }
+  fun predictProbability(X: MultiArray<Double, D2>): MultiArray<Double, D1> {
+    val linearModel = X.dot(weights!!) + bias
+    return sigmoid(linearModel)
+  }
 
-    private fun sigmoid(z: MultiArray<Double, D1>): MultiArray<Double, D1> {
-        return 1.0 / (1.0 + exp(-z))
-    }
+  fun predictProbability(X: DataFrame): MultiArray<Double, D1> {
+    return predictProbability(mk.ndarray(X.columns.map { (it as DataFrame.Column<Double>).values }).transpose())
+  }
+
+  private fun sigmoid(z: MultiArray<Double, D1>): MultiArray<Double, D1> {
+    return 1.0 / (1.0 + exp(-z))
+  }
 }
--- src/commonTest/kotlin/ltd/mbor/sciko/ml/linear/LogisticRegressionTest.kt
+++ src/commonTest/kotlin/ltd/mbor/sciko/ml/linear/LogisticRegressionTest.kt
@@ -4,112 +4,117 @@ import ltd.mbor.sciko.ml.assertEquals
 import ltd.mbor.sciko.ml.dataframe.DataFrame
 import org.jetbrains.kotlinx.multik.api.mk
 import org.jetbrains.kotlinx.multik.api.ndarray
-import org.jetbrains.kotlinx.multik.ndarray.data.get
 import org.jetbrains.kotlinx.multik.ndarray.operations.toList
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class LogisticRegressionTest {
 
-    @Test
-    fun testFit() {
-        val X = mk.ndarray(mk[
-            mk[1.0, 2.0],
-            mk[2.0, 4.0],
-            mk[3.0, 8.0]
-        ])
-        val y = mk.ndarray(mk[0.0, 1.0, 1.0])
+  @Test
+  fun testFit() {
+    val X = mk.ndarray(
+      mk[
+        mk[1.0, 2.0],
+        mk[2.0, 4.0],
+        mk[3.0, 8.0]
+      ]
+    )
+    val y = mk.ndarray(mk[0.0, 1.0, 1.0])
 
-        val model = LogisticRegression().fit(X, y)
+    val model = LogisticRegression().fit(X, y)
 
-        assertEquals(0.0, model.bias, 1e-6)
-        assertEquals(mk.ndarray(mk[0.0, 0.0]), model.weights, 1e-6)
-    }
+    assertEquals(-1.034517, model.bias, 1e-6)
+    assertEquals(mk.ndarray(mk[0.0810849, 0.532951]), model.weights, 1e-6)
+  }
 
-    @Test
-    fun testFitDataFrame() {
-        val csvData = """
-            a,b,c
-            1,2,0
-            2,4,1
-            3,8,1
-        """.trimIndent()
+  @Test
+  fun testFitDataFrame() {
+    val csvData = """
+      a,b,c
+      1,2,0
+      2,4,1
+      3,8,1
+      """.trimIndent()
 
-        val dataFrame = DataFrame.readCsv(csvData)
-        val X = dataFrame.dropColumnsByName("c")
-        val y = dataFrame["c"] as DataFrame.Column<Double>
+    val dataFrame = DataFrame.readCsv(csvData)
+    val X = dataFrame.dropColumnsByName("c")
+    val y = dataFrame["c"] as DataFrame.Column<Double>
 
-        val model = LogisticRegression().fit(X, y)
+    val model = LogisticRegression().fit(X, y)
 
-        assertEquals(0.0, model.bias, 1e-6)
-        assertEquals(mk.ndarray(mk[0.0, 0.0]), model.weights, 1e-6)
-    }
+    assertEquals(-1.034517, model.bias, 1e-6)
+    assertEquals(mk.ndarray(mk[0.0810849, 0.532951]), model.weights, 1e-6)
+  }
 
-    @Test
-    fun testPredict() {
-        val X = mk.ndarray(mk[
-            mk[1.0, 2.0],
-            mk[2.0, 4.0],
-            mk[3.0, 8.0]
-        ])
-        val y = mk.ndarray(mk[0.0, 1.0, 1.0])
+  @Test
+  fun testPredict() {
+    val X = mk.ndarray(
+      mk[
+        mk[1.0, 2.0],
+        mk[2.0, 4.0],
+        mk[3.0, 8.0]
+      ]
+    )
+    val y = mk.ndarray(mk[0.0, 1.0, 1.0])
 
-        val model = LogisticRegression().fit(X, y)
-        val predictions = model.predict(X)
+    val model = LogisticRegression().fit(X, y)
+    val predictions = model.predict(X)
 
-        assertEquals(y, predictions, 1e-6)
-    }
+    assertEquals(y, predictions, 1e-6)
+  }
 
-    @Test
-    fun testPredictDataFrame() {
-        val csvData = """
-            a,b,c
-            1,2,0
-            2,4,1
-            3,8,1
-        """.trimIndent()
+  @Test
+  fun testPredictDataFrame() {
+    val csvData = """
+      a,b,c
+      1,2,0
+      2,4,1
+      3,8,1
+      """.trimIndent()
 
-        val dataFrame = DataFrame.readCsv(csvData)
-        val X = dataFrame.dropColumnsByName("c")
-        val y = dataFrame["c"] as DataFrame.Column<Double>
+    val dataFrame = DataFrame.readCsv(csvData)
+    val X = dataFrame.dropColumnsByName("c")
+    val y = dataFrame["c"] as DataFrame.Column<Double>
 
-        val model = LogisticRegression().fit(X, y)
-        val predictions = model.predict(X)
+    val model = LogisticRegression().fit(X, y)
+    val predictions = model.predict(X)
 
-        assertEquals(y.values, predictions.toList(), 1e-6)
-    }
+    assertEquals(y.values, predictions.toList(), 1e-6)
+  }
 
-    @Test
-    fun testPredictProbability() {
-        val X = mk.ndarray(mk[
-            mk[1.0, 2.0],
-            mk[2.0, 4.0],
-            mk[3.0, 8.0]
-        ])
-        val y = mk.ndarray(mk[0.0, 1.0, 1.0])
+  @Test
+  fun testPredictProbability() {
+    val X = mk.ndarray(
+      mk[
+        mk[1.0, 2.0],
+        mk[2.0, 4.0],
+        mk[3.0, 8.0]
+      ]
+    )
+    val y = mk.ndarray(mk[0.0, 1.0, 1.0])
 
-        val model = LogisticRegression().fit(X, y)
-        val probabilities = model.predictProbability(X)
+    val model = LogisticRegression().fit(X, y)
+    val probabilities = model.predictProbability(X)
 
-        assertEquals(mk.ndarray(mk[0.5, 0.5, 0.5]), probabilities, 1e-6)
-    }
+    assertEquals(mk.ndarray(mk[0.5, 0.5, 0.5]), probabilities, 1e-6)
+  }
 
-    @Test
-    fun testPredictProbabilityDataFrame() {
-        val csvData = """
-            a,b,c
-            1,2,0
-            2,4,1
-            3,8,1
-        """.trimIndent()
+  @Test
+  fun testPredictProbabilityDataFrame() {
+    val csvData = """
+      a,b,c
+      1,2,0
+      2,4,1
+      3,8,1
+      """.trimIndent()
 
-        val dataFrame = DataFrame.readCsv(csvData)
-        val X = dataFrame.dropColumnsByName("c")
-        val y = dataFrame["c"] as DataFrame.Column<Double>
+    val dataFrame = DataFrame.readCsv(csvData)
+    val X = dataFrame.dropColumnsByName("c")
+    val y = dataFrame["c"] as DataFrame.Column<Double>
 
-        val model = LogisticRegression().fit(X, y)
-        val probabilities = model.predictProbability(X)
+    val model = LogisticRegression().fit(X, y)
+    val probabilities = model.predictProbability(X)
 
-        assertEquals(listOf(0.5, 0.5, 0.5), probabilities.toList(), 1e-6)
-    }
+    assertEquals(listOf(0.5, 0.5, 0.5), probabilities.toList(), 1e-6)
+  }
 }
